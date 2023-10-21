d75356dc8e4f42fa572bc048d1a82e166c5df538
Cristian Dreyer <cristianbr.dreyer@gmail.com>
2023-10-21 03:57:33 -0300

    to update

--- Assets/Sources/Player/Projectile/Projectile.cs
+++ Assets/Sources/Player/Projectile/Projectile.cs
@@ -6,6 +6,11 @@ public class Projectile : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 1;
 
+    void Start()
+    {
+        Helpers.ActionCallback(() => Destroy(gameObject), 4);
+    }
+
     void Update()
     {
         Move();
