a1fe3a51296e7eb9a15276422c4cece142646628
caita <CaitaXD@gmail.com>
2023-10-21 03:38:06 -0300

    changed spawner to a abstreact class

--- Assets/Sources/Enemy/EnemySpawner.cs
+++ Assets/Sources/Enemy/EnemySpawner.cs
@@ -1,61 +1,13 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
+using Sources.Enemy;
 using UnityEngine;
-using CDreyer;
-using Unity.VisualScripting;
-using Random = System.Random;
 
-namespace Sources.Enemy
+public sealed class EnemySpawner : Spawner
 {
-    public class EnemySpawner : MonoBehaviour
+    public override void OnAfterSpawn(GameObject instance)
     {
-        public int maxEnemyCount = 10;
-        public float spawnInterval = 1f;
-
-        public float elevation = 1;
-
-        public GameObject enemyPrefab;
-
-        private List<GameObject> _enemies;
-
-        private void Awake()
-        {
-            _enemies = new List<GameObject>();
-        }
-
-        private void Start()
-        {
-            StartCoroutine(WaitAndSpawn());
-        }
-
-        IEnumerator WaitAndSpawn()
-        {
-            while (true)
-            {
-                if (_enemies.Count >= maxEnemyCount)
-                {
-                    yield return null;
-                    continue;
-                }
-
-                yield return Helpers.GetWait(spawnInterval);
-                SpawnEnemy();
-            }
-        }
-
-        private void SpawnEnemy()
-        {
-            int randX = UnityEngine.Random.Range(-10, 10);
-            int randZ = UnityEngine.Random.Range(-10, 10);
-            var randomPosition = new Vector3(randX, elevation, randZ);
-            var newPoint = transform.position + randomPosition;
-            var enemy = Instantiate(enemyPrefab, newPoint, Quaternion.identity);
-            _enemies.Add(enemy);
-
-            var enemyMono = enemy.GetComponent<EnemyMono>();
-            enemyMono.OnDied += () => { _enemies.Remove(enemy); };
-        }
+        var enemy = instance.GetComponent<IEnemy>();
+        enemy.OnDied += () => base.instances.Remove(instance);
     }
 }
