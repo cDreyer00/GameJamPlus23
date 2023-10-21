b54da217ef37f73474ddcedb065f8282f54b8a9c
Cristian Dreyer <cristianbr.dreyer@gmail.com>
2023-10-21 02:18:40 -0300

    player rotation done

--- Assets/Sources/Player/PlayerController.cs
+++ Assets/Sources/Player/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using CDreyer;
 
 public class PlayerController : MonoBehaviour
 {
@@ -12,19 +13,24 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         if (Input.GetMouseButtonUp(0))
+        {
             Shoot();
+            lastMousePos = Vector3.zero;
+        }
 
         if (Input.GetMouseButton(0))
-        {            
             Rotate();
-        }
     }
 
     void Rotate()
     {
+        if (lastMousePos == Vector3.zero)
+            lastMousePos = Input.mousePosition;
+            
         mouseDelta = Input.mousePosition - lastMousePos;
-        model.transform.Rotate(Vector3.forward, mouseDelta.x);
-        
+        GameLogger.Log(mouseDelta.x);
+        model.transform.eulerAngles += Vector3.up * mouseDelta.x;
+
         lastMousePos = Input.mousePosition;
     }
 
