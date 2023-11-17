c3b7ad3f330b204913005d5a7bb3d74bc3d0eab3
cdreyer <cristianbr.dreyer@gmail.com>
2023-11-17 08:31:19 -0300

    old spawns removed

--- Assets/Sources/Systems/SpawnerService.cs
+++
@@ -1,9 +0,0 @@
-public static class SpawnerSrevice
-{
-    static EffectSignSpawner _effecSignSpawner;
-    public static EffectSignSpawner EffectSignSpawner
-    {
-        get => _effecSignSpawner;
-        set => _effecSignSpawner = value;
-    }
-}
