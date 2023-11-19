e880057c965d54a29b2161c30bb2c796002bd9fc
caitaxd <caitaxd@gmail.com>
2023-11-19 18:27:53 -0300

    Charcter IPoolable.OnGet Calls onInitialized Event

--- Assets/Sources/Characters/Character.cs
+++ Assets/Sources/Characters/Character.cs
@@ -8,14 +8,12 @@ using Random = UnityEngine.Random;
 
 public class Character : MonoBehaviour, ICharacter, IPoolable<MonoBehaviour>
 {
-    readonly StateMachine<State>      _stateMachine = new(State.Idle);
-    readonly HashSet<CharacterModule> _modules      = new();
-    readonly CharacterEvents          _events       = new();
+    readonly HashSet<CharacterModule> _modules = new();
+    readonly CharacterEvents          _events  = new();
 
     public string team = "";
     public Vector3 Position => transform.position;
     public CharacterEvents Events => _events;
-    public StateMachine<State> StateMachine => _stateMachine;
     public IReadOnlyCollection<CharacterModule> Modules => _modules;
     public bool AddModule(CharacterModule module) => _modules.Add(module);
     public bool RemoveModule(CharacterModule module) => _modules.Remove(module);
@@ -25,23 +23,11 @@ public class Character : MonoBehaviour, ICharacter, IPoolable<MonoBehaviour>
         module = GetModule<T>();
         return module != null;
     }
-    void Awake()
-    {
-        _stateMachine.SetSubState(State.InControl, State.Idle);
-        _stateMachine.SetSubState(State.InControl, State.Chasing);
-        _stateMachine.SetSubState(State.InControl, State.Attacking);
-        _stateMachine.SetSubState(State.Yielded, State.Controlled);
-        _stateMachine.SetSubState(State.Yielded, State.Dying);
-    }
     void Start()
     {
         _events.onDied += OnDied;
     }
     void OnDied(ICharacter character) => Pool.Release(this);
-    void Update()
-    {
-        _stateMachine.Update();
-    }
     public enum State
     {
         // InControl:
@@ -55,7 +41,10 @@ public class Character : MonoBehaviour, ICharacter, IPoolable<MonoBehaviour>
         Dying,
     }
     public GenericPool<MonoBehaviour> Pool { get; set; }
-    public void OnGet() {}
+    public void OnGet()
+    {
+        _events.onInitialized?.Invoke();
+    }
     public void OnRelease() {}
     public void OnCreated() {}
 }
---
+++ Assets/Sources/Characters/Modules/StateModule.cs
@@ -0,0 +1,10 @@
+﻿using Sources.Systems.FSM;
+
+namespace Sources.Characters.Modules
+{
+    public class StateModule : CharacterModule
+    {
+        StateMachine<Character.State> _stateMachine = new(Character.State.Idle);
+        protected override void Init() {}
+    }
+}
