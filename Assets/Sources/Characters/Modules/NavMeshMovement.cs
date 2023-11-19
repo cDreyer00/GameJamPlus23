5ae13e00df190cfd895ec00324517b19d360bd96
caitaxd <caitaxd@gmail.com>
2023-11-19 18:29:25 -0300

    Added State Module and updated NavMehsMovement to require it

--- Assets/Sources/Characters/Modules/NavMeshMovement.cs
+++ Assets/Sources/Characters/Modules/NavMeshMovement.cs
@@ -1,12 +1,15 @@
 using System;
+using Sources.Characters.Modules;
 using UnityEngine;
 using UnityEngine.AI;
 using Sources.Systems.FSM;
 using static Character;
 
-[RequireComponent(typeof(NavMeshAgent))]
+[RequireComponent(typeof(NavMeshAgent)), RequireComponent(typeof(StateModule))]
 public class NavMeshMovement : CharacterModule, IMovementModule
 {
+    bool _registered;
+
     [SerializeField] NavMeshAgent agent;
     [SerializeField] Transform    target;
     [SerializeField] float        startCooldownTime;
@@ -22,13 +25,16 @@ public class NavMeshMovement : CharacterModule, IMovementModule
     }
     void Start()
     {
-        var fsm = Character.StateMachine;
-        fsm.Transition(State.Idle, () => target == null);
-        fsm.Transition(State.Idle, State.Chasing, () => target != null);
-        fsm[LifeCycle.Enter, State.Chasing] += () => agent.isStopped = false;
-        fsm[LifeCycle.Exit, State.Chasing] += () => agent.isStopped = true;
-        fsm[LifeCycle.Update, State.Chasing] += () => SetDestination(target.position);
-        Helpers.Delay(startCooldownTime, static self => self.SetTarget(GameManager.Instance.Player.transform), this);
+        if (!_registered) {
+            var fsm = Character.GetModule<StateModule>().StateMachine;
+            fsm.Transition(State.Idle, () => target == null);
+            fsm.Transition(State.Idle, State.Chasing, () => target != null);
+            fsm[LifeCycle.Enter, State.Chasing] += () => agent.isStopped = false;
+            fsm[LifeCycle.Exit, State.Chasing] += () => agent.isStopped = true;
+            fsm[LifeCycle.Update, State.Chasing] += () => SetDestination(target.position);
+            Helpers.Delay(startCooldownTime, static self => self.SetTarget(GameManager.Instance.Player.transform), this);
+            _registered = true;
+        }
     }
     protected override void Init()
     {
