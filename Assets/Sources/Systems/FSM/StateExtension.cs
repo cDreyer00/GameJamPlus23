e23887a69d7fdfb8a66c2c1dd3fb1e5730d23855
caitaxd <caitaxd@gmail.com>
2023-11-18 22:37:51 -0300

    Register delegates from = to +=

--- Assets/Sources/Systems/FSM/StateExtension.cs
+++ Assets/Sources/Systems/FSM/StateExtension.cs
@@ -7,10 +7,10 @@ namespace Sources.Systems.FSM
         public static void Register<TEnum>(this IState<TEnum> state, StateMachine<TEnum> stateMachine) where TEnum : Enum
         {
             var stateEnum = state.StateEnum;
-            stateMachine[LifeCycle.Enter, stateEnum] = state.Enter;
-            stateMachine[LifeCycle.FixedUpdate, stateEnum] = state.FixedUpdate;
-            stateMachine[LifeCycle.Update, stateEnum] = state.Update;
-            stateMachine[LifeCycle.Exit, stateEnum] = state.Exit;
+            stateMachine[LifeCycle.Enter, stateEnum] += state.Enter;
+            stateMachine[LifeCycle.FixedUpdate, stateEnum] += state.FixedUpdate;
+            stateMachine[LifeCycle.Update, stateEnum] += state.Update;
+            stateMachine[LifeCycle.Exit, stateEnum] += state.Exit;
         }
     }
 }
